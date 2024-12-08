43ad3cd225c906672805485fe681891b3ab882b6
Simon Parten <quafadas@gmail.com>
2024-12-08 20:47:00 +0100

    each test suite compiles into native

--- test/src/mill/main/client/ProxyStreamTests.test.scala
+++ test/src/mill/main/client/ProxyStreamTests.test.scala
@@ -1,117 +1,130 @@
-// import java.io._
+import java.io._
 
-// import utest._
-// import mill.main.client.ProxyStream
+import utest._
+import mill.main.client.ProxyStream
+import scala.concurrent.Future
 
-// class TeeOutputStream(out1: OutputStream, out2: OutputStream) extends OutputStream {
-//   override def write(b: Int): Unit = {
-//     out1.write(b)
-//     out2.write(b)
-//   }
+class TeeOutputStream(out1: OutputStream, out2: OutputStream) extends OutputStream {
+  override def write(b: Int): Unit = {
+    out1.write(b)
+    out2.write(b)
+  }
 
-//   override def write(b: Array[Byte]): Unit = {
-//     out1.write(b)
-//     out2.write(b)
-//   }
+  override def write(b: Array[Byte]): Unit = {
+    out1.write(b)
+    out2.write(b)
+  }
 
-//   override def write(b: Array[Byte], off: Int, len: Int): Unit = {
-//     out1.write(b, off, len)
-//     out2.write(b, off, len)
-//   }
+  override def write(b: Array[Byte], off: Int, len: Int): Unit = {
+    out1.write(b, off, len)
+    out2.write(b, off, len)
+  }
 
-//   override def flush(): Unit = {
-//     out1.flush()
-//     out2.flush()
-//   }
+  override def flush(): Unit = {
+    out1.flush()
+    out2.flush()
+  }
 
-//   override def close(): Unit = {
-//     try out1.close()
-//     finally out2.close()
-//   }
-// }
+  override def close(): Unit = {
+    try out1.close()
+    finally out2.close()
+  }
+}
 
-// object ProxyStreamTests extends TestSuite {
+object ProxyStreamTests extends TestSuite {
+  implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
 
-//   val tests = Tests {
-//     test("fuzzTests") {
-//       val interestingLengths = Array(
-//         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 100, 126, 127, 128, 129, 130, 253, 254, 255, 256, 257, 1000, 2000, 4000, 8000
-//       )
-//       val interestingBytes = Array[Byte](
-//         -1, -127, -126, -120, -100, -80, -60, -40, -20, -10, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 10, 20, 40, 60, 80, 100, 120, 125, 126, 127
-//       )
+  val tests = Tests {
+    test("fuzzTests") {
+      val interestingLengths = Array(
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 100, 126, 127, 128, 129, 130, 253, 254, 255, 256, 257, 1000, 2000, 4000, 8000
+      )
+      val interestingBytes = Array[Byte](
+        -1, -127, -126, -120, -100, -80, -60, -40, -20, -10, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 10, 20, 40, 60, 80, 100, 120, 125, 126, 127
+      )
 
-//       for (n <- interestingLengths) {
-//         println(s"ProxyStreamTests fuzzing length $n")
-//         for (r <- 1 until interestingBytes.length + 1) {
-//           val outData = Array.ofDim[Byte](n)
-//           val errData = Array.ofDim[Byte](n)
+      for (n <- interestingLengths) {
+        println(s"ProxyStreamTests fuzzing length $n")
+        for (r <- 1 until interestingBytes.length + 1) {
+          val outData = Array.ofDim[Byte](n)
+          val errData = Array.ofDim[Byte](n)
 
-//           for (j <- 0 until n) {
-//             outData(j) = interestingBytes((j + r) % interestingBytes.length)
-//             errData(j) = (-interestingBytes((j + r) % interestingBytes.length)).toByte
-//           }
+          for (j <- 0 until n) {
+            outData(j) = interestingBytes((j + r) % interestingBytes.length)
+            errData(j) = (-interestingBytes((j + r) % interestingBytes.length)).toByte
+          }
 
-//           test0(outData, errData, r, gracefulEnd = false)
-//           test0(outData, errData, r, gracefulEnd = true)
-//         }
-//       }
-//     }
-//   }
+          test0(outData, errData, r, gracefulEnd = false)
+          test0(outData, errData, r, gracefulEnd = true)
+        }
+      }
+    }
+  }
 
-//   def test0(outData: Array[Byte], errData: Array[Byte], repeats: Int, gracefulEnd: Boolean): Unit = {
-//     val pipedOutputStream = new PipedOutputStream()
-//     val pipedInputStream = new PipedInputStream(1000000)
-//     pipedInputStream.connect(pipedOutputStream)
+  def test0(outData: Array[Byte], errData: Array[Byte], repeats: Int, gracefulEnd: Boolean): Unit = {
+    println("WARNING: Original implementation is a PipedOutputStream and PipedInputStream, which is not supported in Scala Native")
+    println("My (extremely naive) implementation is a ByteArrayOutputStream and ByteArrayInputStream")
+    println("Which may be utterly wrong ... ")
+    val pipedOutputStream = new ByteArrayOutputStream()
+    val pipedInputStream = new ByteArrayInputStream(new Array[Byte](1000000))
 
-//     val srcOut = new ProxyStream.Output(pipedOutputStream, ProxyStream.OUT)
-//     val srcErr = new ProxyStream.Output(pipedOutputStream, ProxyStream.ERR)
+    val writer = Future {
+      val writerStream = new DataOutputStream(pipedOutputStream)
+      for (i <- 1 to 10) {
+        writerStream.writeInt(i)
+        Thread.sleep(100) // Simulate some delay
+      }
+      writerStream.close()
+    }
 
-//     val destOut = new ByteArrayOutputStream()
-//     val destErr = new ByteArrayOutputStream()
-//     val destCombined = new ByteArrayOutputStream()
+    val srcOut = new ProxyStream.Output(pipedOutputStream, ProxyStream.OUT)
+    val srcErr = new ProxyStream.Output(pipedOutputStream, ProxyStream.ERR)
 
-//     val pumper = new ProxyStream.Pumper(
-//       pipedInputStream,
-//       new TeeOutputStream(destOut, destCombined),
-//       new TeeOutputStream(destErr, destCombined)
-//     )
+    val destOut = new ByteArrayOutputStream()
+    val destErr = new ByteArrayOutputStream()
+    val destCombined = new ByteArrayOutputStream()
 
-//     val writerThread = new Thread(() => {
-//       try {
-//         for (_ <- 0 until repeats) {
-//           srcOut.write(outData)
-//           srcErr.write(errData)
-//         }
-//         if (gracefulEnd) ProxyStream.sendEnd(pipedOutputStream)
-//         else pipedOutputStream.close()
-//       } catch {
-//         case e: Exception => e.printStackTrace()
-//       }
-//     })
+    val pumper = new ProxyStream.Pumper(
+      pipedInputStream,
+      new TeeOutputStream(destOut, destCombined),
+      new TeeOutputStream(destErr, destCombined)
+    )
 
-//     val pumperThread = new Thread(pumper)
+    val writerThread = new Thread(() => {
+      try {
+        for (_ <- 0 until repeats) {
+          srcOut.write(outData)
+          srcErr.write(errData)
+        }
+        if (gracefulEnd) ProxyStream.sendEnd(pipedOutputStream)
+        else pipedOutputStream.close()
+      } catch {
+        case e: Exception => e.printStackTrace()
+      }
+    })
 
-//     writerThread.start()
-//     pumperThread.start()
+    val pumperThread = new Thread(pumper)
 
-//     writerThread.join()
-//     pumperThread.join()
+    writerThread.start()
+    pumperThread.start()
 
-//     val repeatedOutData = repeatArray(outData, repeats)
-//     val repeatedErrData = repeatArray(errData, repeats)
-//     val combinedData = repeatArray(outData ++ errData, repeats)
+    writerThread.join()
+    pumperThread.join()
 
-//     assert(destOut.toByteArray.sameElements(repeatedOutData))
-//     assert(destErr.toByteArray.sameElements(repeatedErrData))
-//     assert(destCombined.toByteArray.sameElements(combinedData))
-//   }
+    val repeatedOutData = repeatArray(outData, repeats)
+    val repeatedErrData = repeatArray(errData, repeats)
+    val combinedData = repeatArray(outData ++ errData, repeats)
 
-//   def repeatArray(original: Array[Byte], n: Int): Array[Byte] = {
-//     val result = Array.ofDim[Byte](original.length * n)
-//     for (i <- 0 until n) {
-//       System.arraycopy(original, 0, result, i * original.length, original.length)
-//     }
-//     result
-//   }
-// }
+    assert(destOut.toByteArray.sameElements(repeatedOutData))
+    assert(destErr.toByteArray.sameElements(repeatedErrData))
+    assert(destCombined.toByteArray.sameElements(combinedData))
+  }
+
+  def repeatArray(original: Array[Byte], n: Int): Array[Byte] = {
+    val result = Array.ofDim[Byte](original.length * n)
+    for (i <- 0 until n) {
+      System.arraycopy(original, 0, result, i * original.length, original.length)
+    }
+    result
+  }
+}
