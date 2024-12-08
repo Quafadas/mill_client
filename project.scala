c4aa21e7f62cce268ae54c04a9469fb5960d99ee
Simon Parten <quafadas@gmail.com>
2024-12-08 18:39:10 +0100

    these tests pass

---
+++ project.scala
@@ -0,0 +1,6 @@
+//> using platform jvm
+//> using scala 2.13.14
+
+//> using test.dep com.github.sbt:junit-interface:0.13.3
+//> using dep commons-io:commons-io:2.11.0
+//> using resourceDir ./resources
